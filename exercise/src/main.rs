e6b99ba6ad2a94e52987d57490e68e4a66258800
simonjisu <simonjisu@gmail.com>
2023-01-12 02:06:36 +0900

    update

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,61 +1,10 @@
-// struct ImportantExcerpt<'a> {
-//     part: &'a str,
-// }
-
-// impl<'a> ImportantExcerpt<'a> {
-//     fn level(&'a self) -> i32 {
-//         3
-//     }
-// }
-
-// impl<'a> ImportantExcerpt<'a> {
-//     fn announce_and_return_part<'b>(&'a self, announcement: &'b str) -> &'a str {
-//         println!("Attention please: {}", announcement);
-//         self.part
-//     }
-// }
-
-// fn main() {
-//     let novel = String::from("Call me Ishmael. Some years ago...");
-//     let first_sentence = novel.split('.').next().expect("Could not find a '.'");
-//     let i = ImportantExcerpt {
-//         part: first_sentence,
-//     };
-//     println!("{:?}", i.level());
-//     println!("{:?}", i.announce_and_return_part("abc"));
-// }
-    
-// fn print_type_of<T: ?Sized>(_: &T) {
-//     println!("{}", std::any::type_name::<T>())
-// }
-
-
-use std::fmt::Display;
-
-fn longest_with_an_announcement<'a, T>(  // Lifetime 정의, Trait 정의
-    x: &'a str,
-    y: &'a str,
-    ann: T,
-) -> &'a str
-where   
-    T: Display,
-{
-    println!("Announcement! {}", ann);
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+enum List {
+    Cons(i32, List),
+    Nil,
 }
 
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+use crate::List::{Cons, Nil};
 
-    let result = longest_with_an_announcement(
-        string1.as_str(),
-        string2,
-        "Today is someone's birthday!",
-    );
-    println!("The longest string is {}", result);
+fn main() {
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
